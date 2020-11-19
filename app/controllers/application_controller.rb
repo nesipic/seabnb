3e0febb992936104c21d4be7ba628e558087b020
Britt Tarien <britt.tarien@gmail.com>
2020-11-19 15:15:35 +0100

    user can edit profile and upload avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :picture])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  validates :username, presence: false, uniqueness: true
   has_many :bookings
   has_many :owned_boats, class_name: 'Boat'
   has_many :rented_boats, through: :bookings, source: :boat
